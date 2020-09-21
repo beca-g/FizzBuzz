32aa38b6cd0499b8081f900ad4344d09a12df1f1
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-09-21 18:12:42 +0100

    Final commit

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,19 @@
-require_relative '../lib/fizzbuzz'
+require './fizzbuzz'
 
 describe "fizzbuzz" do
-  it 'returns "fizz" when passes 3' do
+  it 'returns "fizz" when divisable by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passes 5' do
+  it 'returns "buzz" when divisable by 5' do
   	expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" a multiple of both 3 and 5' do
+  it 'returns "fizzbuzz" when divisable by both 3 and 5' do
   	expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
-  it 'returns self when passed a num that is not a multiple of 3 or 5' do
+  it 'returns the number when passed a num that is not a multiple of 3 or 5' do
   	expect(fizzbuzz(4)).to eq 4
+  end
+  it 'returns "Not a number" when passed anything but a number' do
+    expect(fizzbuzz("dog")).to eq 'Not a number'
   end
 end
